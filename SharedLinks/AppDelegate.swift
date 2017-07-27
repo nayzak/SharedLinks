93e4cb9b3e83cb5f92c55a201bffb20a0c58ae96
Ian Kazlauskas <nayzak90@gmail.com>
2017-07-27 16:38:20 +0900

    Initial Commit

---
+++ SharedLinks/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SharedLinks
+//
+//  Created by Ian Kazlauskas on 27/07/2017.
+//  Copyright © 2017 Ian Kazlauskas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
