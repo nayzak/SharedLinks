f77ef6b251e04b9bd319e30bb92eeea15580be5f
Ian Kazlauskas <nayzak90@gmail.com>
2017-07-30 15:19:17 +0900

    added popover

--- SharedLinks/SupportingFiles/AppDelegate.swift
+++ SharedLinks/SupportingFiles/AppDelegate.swift
@@ -10,22 +10,42 @@ import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-  var window: NSWindow!
+  var popover: NSPopover!
   var menuBarItem: NSStatusItem!
 
   func applicationDidFinishLaunching(_ notification: Notification) {
 
-    menuBarItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength).apply {
-      $0.button?.image = #imageLiteral(resourceName: "menu-bar-icon")
+    popover = NSPopover().apply {
+      $0.contentViewController = LinksListViewController()
+      $0.animates = false
     }
-    
-    let linksViewController = LinksListViewController()
-    window = NSWindow(contentViewController: linksViewController)
-    window.makeKeyAndOrderFront(nil)
+
+    menuBarItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength).apply {
+      $0.button?.apply {
+        $0.image = #imageLiteral(resourceName: "menu-bar-icon")
+        $0.action = #selector(togglePopover(_:))
+      }
+    }
   }
 
   func applicationWillTerminate(_ notification: Notification) {
     // Insert code here to tear down your application
   }
-}
 
+  @objc private func togglePopover(_ sender: Any?) {
+    if popover.isShown {
+      closePopover()
+    } else {
+      showPopover()
+    }
+  }
+
+  private func showPopover() {
+    guard let button = menuBarItem.button else { return }
+    popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+  }
+
+  private func closePopover() {
+    popover.performClose(nil)
+  }
+}
