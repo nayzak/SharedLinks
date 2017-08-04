70da4e27a115b9b2c7c3543bf26737716dea36f9
Ian Kazlauskas <nayzak90@gmail.com>
2017-08-04 13:53:34 +0900

    improved atom parcer

--- SharedLinks/DataSources/RSS/Link+AtomFeed.swift
+++ SharedLinks/DataSources/RSS/Link+AtomFeed.swift
@@ -12,8 +12,17 @@ import FeedKit
 extension Link {
 
   init?(atomFeedEntry item: AtomFeedEntry, author: Author) {
+    guard let links = item.links?.flatMap({ $0.attributes }), !links.isEmpty else { return nil }
+    
+    var href: String?
+    if links.count == 1 {
+      href = links.first?.href
+    } else {
+      href = links.first { $0.rel != nil && $0.rel! == "alternate" }?.href
+    }
+    
     guard
-      let url = item.links?.first?.attributes?.href.flatMap(URL.init(string:)),
+      let url = href.flatMap(URL.init(string:)),
       let text = item.content?.value,
       let publishDate = item.published ?? item.updated
       else { return nil }
